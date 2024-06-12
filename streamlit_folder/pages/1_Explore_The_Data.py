b87c5b8c28e41f4cd7f85b58595ed542ad97637d
Michael Tatar <michael.tatar.jr@gmail.com>
2024-06-11 21:27:19 -0500

    latest

--- streamlit_folder/pages/1_Explore_The_Data.py
+++ streamlit_folder/pages/1_Explore_The_Data.py
@@ -23,7 +23,7 @@ Mean Number of Words per Document: 25591.0
 Vocabulary Size: 44277 """
 )
 
-path = Path(__file__).parent
+path = Path(__file__).parent.parent
 
 image1 = Image.open(path /'pngs/charactercount.png')
 image2 = Image.open(path /'pngs/timeline.png')
